b6bd01f1ee1ab39e69e46c3f9c91dd1af348b8d0
Omar O'Sullivan <oos@mac.lan>
2025-09-04 00:14:35 +0100

    Update TinaCMS config with hardcoded credentials for troubleshooting

--- tina/config.ts
+++ tina/config.ts
@@ -11,9 +11,9 @@ export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "77bf5b42-8901-4e40-9898-9d729aeb0d38",
   // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  token: process.env.TINA_TOKEN || "404a1429bc0efac7cf5fd77b55716e967b3af455",
 
   build: {
     outputFolder: "admin",
